f98edb418565619f2a5458cd6ed7034e02d640a5
XephyrCM <Rcolemoore@gmail.com>
2023-02-06 08:02:47 -0500

    Create M03 Lab Case Study.py

---
+++ M03 Lab Case Study.py
@@ -0,0 +1,31 @@
+class Vehicle:
+    def __init__(self, v_type):
+        self.vehicle_type = v_type
+
+class Automobile(Vehicle):
+    def __init__(self, year, make, model, doors, roof):
+        super().__init__("car")
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+def main():
+    year = input("Enter the year of the car: ")
+    make = input("Enter the make of the car: ")
+    model = input("Enter the model of the car: ")
+    doors = input("Enter the number of doors (2 or 4): ")
+    roof = input("Enter the type of roof (solid or sun roof): ")
+
+    car = Automobile(year, make, model, doors, roof)
+
+    print("Vehicle type:", car.vehicle_type)
+    print("Year:", car.year)
+    print("Make:", car.make)
+    print("Model:", car.model)
+    print("Number of doors:", car.doors)
+    print("Type of roof:", car.roof)
+
+if __name__ == '__main__':
+    main()
